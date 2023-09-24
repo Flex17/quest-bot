a77dbb88560f3cf76b915a0593c71400f6f575dc
github-84616532 <github-84616532>
2023-09-24 10:26:56 +0000

    Edited files:     /src/functions.js

--- src/functions.js
+++ src/functions.js
@@ -6,11 +6,11 @@ function playGame() {
     var isWinned = getRandomInt(2) % 2 === 1;
     
     if (isWinned) {
-        $jsapi.context().coins *= 2;
-        return 'Вы победили, количество ваших очков ' + $jsapi.context().coins;
+        $jsapi.context.coins *= 2;
+        return 'Вы победили, количество ваших очков ' + $jsapi.context.coins;
     }
     
-    $jsapi.context().coins = 0;
+    $jsapi.context.coins = 0;
     
     return 'Вы проиграли, количество ваших очков равно 0';
 }
