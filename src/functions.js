d9b98ef596951b746f518e5a720f80db7b9b4478
github-84616532 <github-84616532>
2023-09-24 10:07:34 +0000

    Edited files:     /src/functions.js     /src/main.sc

--- src/functions.js
+++ src/functions.js
@@ -1,3 +1,16 @@
 function getRandomInt(max) {
   return Math.floor(Math.random() * max);
 }
+
+function playGame() {
+    var isWinned = getRandomInt(1) > 0.5;
+    
+    if (isWinned) {
+        $context.coins *= 2;
+        return `Вы победили, количество ваших очков ${$context.coins}`;
+    }
+    
+    $context.coins = 0;
+    
+    return 'Вы проиграли, количество ваших очков равно 0';
+}
